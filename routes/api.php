235774c6a36817fc9eec3fab840a43f05fd13e82
tranminhbgs2 <tranvanminh30398@gmail.com>
2019-09-19 01:06:35 +0700

    review created

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('/products','ProductController');
 //Review
 Route::group(['prefix'=>'products'],function (){
-   Route::apiResource('/{products}/reviews','ReviewController');
+   Route::apiResource('/{product}/reviews','ReviewController');
 });
